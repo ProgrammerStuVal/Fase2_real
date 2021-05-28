9d80fc353fa9828c977eb3f7fbd0a6c50277a046
Programmer666 <nick@de-pijper.nl>
2021-05-28 02:48:17 -0700

    Bin configuration altered. from 1 to 6. With that all the camera's

--- ariac_behaviors/unit_1_behaviors/unit_1_flexbe_behaviors/src/unit_1_flexbe_behaviors/pick_part_from_bin_sm.py
+++ ariac_behaviors/unit_1_behaviors/unit_1_flexbe_behaviors/src/unit_1_flexbe_behaviors/pick_part_from_bin_sm.py
@@ -8,10 +8,11 @@
 ###########################################################
 
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
-from ariac_flexbe_states.end_assignment_state import EndAssignment
+from ariac_flexbe_states.lookup_from_table import LookupFromTableState
 from ariac_flexbe_states.moveit_to_joints_dyn_ariac_state import MoveitToJointsDynAriacState
 from ariac_flexbe_states.srdf_state_to_moveit_ariac_state import SrdfStateToMoveitAriac
-from ariac_flexbe_states.start_assignment_state import StartAssignment
+from ariac_logistics_flexbe_states.get_material_locations import GetMaterialLocationsState
+from ariac_support_flexbe_states.get_item_from_list_state import GetItemFromListState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -46,7 +47,7 @@ class pick_part_from_binSM(Behavior):
 
 
 	def create(self):
-		# x:113 y:455, x:130 y:365
+		# x:113 y:455, x:531 y:244
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['part', 'robot_namespace'])
 		_state_machine.userdata.part = ''
 		_state_machine.userdata.robot_namespace = ''
@@ -61,6 +62,7 @@ class pick_part_from_binSM(Behavior):
 		_state_machine.userdata.action_topic_out = ''
 		_state_machine.userdata.joint_values = []
 		_state_machine.userdata.joint_names = []
+		_state_machine.userdata.zero = 0
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -69,31 +71,47 @@ class pick_part_from_binSM(Behavior):
 
 
 		with _state_machine:
-			# x:20 y:64
-			OperatableStateMachine.add('start',
-										StartAssignment(),
-										transitions={'continue': 'movePre'},
-										autonomy={'continue': Autonomy.Off})
+			# x:32 y:166
+			OperatableStateMachine.add('GetPartLocation',
+										GetMaterialLocationsState(),
+										transitions={'continue': 'GetBinFromLocations'},
+										autonomy={'continue': Autonomy.Off},
+										remapping={'part': 'part', 'material_locations': 'locations'})
 
-			# x:250 y:323
-			OperatableStateMachine.add('enf',
-										EndAssignment(),
-										transitions={'continue': 'finished'},
-										autonomy={'continue': Autonomy.Off})
+			# x:644 y:15
+			OperatableStateMachine.add('LookupCameraFrame',
+										LookupFromTableState(parameter_name='/ariac_tables', table_name='bin_configuration', index_title='bin', column_title='camera_frame'),
+										transitions={'found': 'movePre', 'not_found': 'failed'},
+										autonomy={'found': Autonomy.Off, 'not_found': Autonomy.Off},
+										remapping={'index_value': 'bin', 'column_value': 'camera_frame'})
 
-			# x:147 y:68
+			# x:449 y:15
+			OperatableStateMachine.add('LookupCameraTopic',
+										LookupFromTableState(parameter_name='/ariac_tables', table_name='bin_configuration', index_title='bin', column_title='camera_topic'),
+										transitions={'found': 'LookupCameraFrame', 'not_found': 'LookupCameraTopic'},
+										autonomy={'found': Autonomy.Off, 'not_found': Autonomy.Off},
+										remapping={'index_value': 'bin', 'column_value': 'camera_topic'})
+
+			# x:836 y:241
+			OperatableStateMachine.add('MoveToBin',
+										MoveitToJointsDynAriacState(),
+										transitions={'reached': 'finished', 'planning_failed': 'failed', 'control_failed': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
+										remapping={'action_topic_namespace': 'action_topic_namespace', 'move_group': 'move_group', 'action_topic': 'action_topic', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:839 y:125
 			OperatableStateMachine.add('movePre',
 										SrdfStateToMoveitAriac(),
 										transitions={'reached': 'MoveToBin', 'planning_failed': 'failed', 'control_failed': 'failed', 'param_error': 'failed'},
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
 										remapping={'config_name': 'config_name', 'move_group': 'move_group', 'action_topic_namespace': 'action_topic_namespace', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
 
-			# x:326 y:90
-			OperatableStateMachine.add('MoveToBin',
-										MoveitToJointsDynAriacState(),
-										transitions={'reached': 'enf', 'planning_failed': 'failed', 'control_failed': 'failed'},
-										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
-										remapping={'action_topic_namespace': 'action_topic_namespace', 'move_group': 'move_group', 'action_topic': 'action_topic', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+			# x:238 y:122
+			OperatableStateMachine.add('GetBinFromLocations',
+										GetItemFromListState(),
+										transitions={'done': 'LookupCameraTopic', 'invalid_index': 'GetBinFromLocations'},
+										autonomy={'done': Autonomy.Off, 'invalid_index': Autonomy.Off},
+										remapping={'list': 'locations', 'index': 'zero', 'item': 'bin'})
 
 
 		return _state_machine
